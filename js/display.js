cf8ad7577d27924e4c757e1b4965ac28832d10ee
Patrick <dmtinfernocide@gmail.com>
2015-09-17 17:12:49 -0500

    Create display.js

---
+++ js/display.js
@@ -0,0 +1,43 @@
+
+"use strict";
+
+var display = {};
+
+display.init = function() {
+ display.canvas = document.getElementById("display");
+ display.context = display.canvas.getContext("2d");
+
+ display.width = display.canvas.width;
+ display.height = display.canvas.height;
+
+ display.scale = 16;
+};
+
+display.draw = function() {
+ display.update();
+
+ display.clear();
+ display.drawMap();
+};
+
+display.update = function() {
+ display.canvas.width = window.innerWidth-42-342;
+ display.canvas.height = window.innerHeight-42;
+
+ display.width = display.canvas.width;
+ display.height = display.canvas.height;
+};
+
+display.clear = function() {
+ display.context.fillStyle = "#000000";
+ display.context.fillRect(0,0,display.width,display.height);
+};
+
+display.drawMap = function() {
+ for(var i=0;i<map.size.x;i++) {
+  for(var j=0;j<map.size.y;j++) {
+	  display.context.drawImage(map.tileSet, 0, 0, 16, 16, i*16, j*16, display.scale, display.scale);
+  }
+ }
+
+};
