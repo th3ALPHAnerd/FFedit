728ef8ef0573d518ef5aa873b80912fd55f155fa
Patrick <dmtinfernocide@gmail.com>
2015-10-01 19:14:11 -0500

    Update tile.js

--- js/tile.js
+++ js/tile.js
@@ -3,18 +3,44 @@
 var tile = {};
 
 tile.init = function() {
-	tile.container = document.getElementById("tileList");
-	tile.buildList("img/test.png",128,128);
+	tile.res = 72;  //Resolution of a single tile
+	tile.sheetRes = 936;//Resolution of the full sheet
+  tile.container = document.getElementById("tileList");
+  tile.container.innerHTML = tile.buildList("img/test.png",tile.res,tile.res,tile.sheetRes);
+  tile.selected = 0;
 };
 
-tile.buildList = function(src,x,y) {
-	var s = 1000;
-	var tonsOfHtml = "<div style='overflow: hidden; width: " + s + "px; height: " + s + "px;' onclick='console.log(\"I pushed the button numero " + i + "," + j + "!\");'>";
-	for(var i=0;i<x/s;i++) {
-		for(var j=0;j<y/s;j++) {
-			tonsOfHtml += "<img src='" + src + "' ></img>";
-		}
+tile.buildList = function(src,x,y,size) {
+  var width = x*4+16;
+  var tonsOfHtml = "<div class='wrapper' style='width:" + width + "px;'>";
+  var k = 0;
+  for(var i=0;i<size/y;i++) {
+    for(var j=0;j<size/x;j++) {
+      tonsOfHtml += "<div onclick='window.tile.selectTile(this,"+k+")'class='cell' style='width: " + x + "px; height: " + y + "px;'>" +
+                    "<img style='margin-left: -" + j*x + ";margin-top:-" + i*y + "' src='" + src + "' ></img></div>";
+      k++;
+    }
   }
-  tonsOfHtml += "</div>"
-  tile.container.innerHTML = tonsOfHtml;
+  tonsOfHtml += "</div>";
+  return tonsOfHtml;
+};
+
+tile.selectTile = function(element,k) {
+  var previousSelected = document.getElementsByClassName('selected');
+  for(var i=0;i<previousSelected.length;i++) {
+    previousSelected[i].className = 'cell';
+  }
+  element.className = 'selected';
+	tile.selected = k;
+};
+
+//Takes the index of a tile and gives the top left corner of it. Tile 0 is 0,0 and tile 1 is 16,0... etc...
+tile.getTileByIndex = function(k) {
+	for(var i=0;i<(tile.sheetRes/tile.res);i++) {
+		for(var j=0;j<(tile.sheetRes/tile.res);j++) {
+			if(k === 0)
+				return {x: j, y: i};
+			k--;
+		}
+	}
 };
