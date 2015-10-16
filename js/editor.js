6723de08fce6c35dd0bddec64e9e5f780476a318
Patrick <dmtinfernocide@gmail.com>
2015-10-16 16:38:58 -0500

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -3,11 +3,39 @@
 var editor = {};
 
 editor.init = function() {
+	//Tool IDs:
+	//0 - paint
+	//1 - select
+	//2 - fill
+	editor.tool = 0; editor.direction = 0;
+	editor.rPush = false;
+	editor.toolIcons = window.image.get('img/tools.png');
+	editor.dirIcons = window.image.get('img/direction.png');
+
 	editor.resizeMap();
 };
 
 editor.step = function() {
+	if(input.keys[81])
+		editor.tool = 0;
+  else if(input.keys[69])
+    editor.tool = 1;
+  else if(input.keys[70])
+    editor.tool = 2;
 
+  if(input.keys[82] && !editor.rPush) {
+		editor.direction = editor.direction > 2 ? 0 : editor.direction+1;
+		editor.rPush = true;
+	}
+	else if(!input.keys[82] && editor.rPush) {
+		editor.rPush = false;
+	}
+};
+
+//Draws the current tool/settings in top right corner.
+editor.draw = function() {
+	display.context.drawImage(editor.toolIcons, (editor.tool*tile.res), 0, tile.res, tile.res, display.width-tile.res, 0, tile.res, tile.res);
+	display.context.drawImage(editor.dirIcons, (editor.direction*tile.res), 0, tile.res, tile.res, display.width-(tile.res*2), 0, tile.res, tile.res);
 };
 
 editor.resizeMap = function() {
@@ -19,7 +47,8 @@ editor.onMouse = function(pos,mvmnt,lmb,rmb,mmb) {
  if(lmb === true) {
 	 var res = editor.getTileByRelativeCoords(pos);
 	 if(map.inBounds(res)) {
-		map.data[res.x][res.y] = tile.selected;
+		map.data[res.x][res.y].tile = tile.selected;
+		map.data[res.x][res.y].r = editor.direction;
 	 }
  }
  if(rmb && !input.keys[16]) {
@@ -37,7 +66,7 @@ editor.onMouse = function(pos,mvmnt,lmb,rmb,mmb) {
 
 //Takes a screen relative point {x, y} and returns the tile {x, y} that is on.
 editor.getTileByRelativeCoords = function(p) {
-	var x = Math.floor((p.x+display.offset.x)/display.scale);
-	var y = Math.floor((p.y+display.offset.y)/display.scale);
+	var x = Math.floor((p.x+display.offset.x+(display.scale/2))/display.scale);
+	var y = Math.floor((p.y+display.offset.y+(display.scale/2))/display.scale);
 	return {x: x, y: y};
 }
